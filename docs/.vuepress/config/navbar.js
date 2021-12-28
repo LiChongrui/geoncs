295f59b4330c532df60ad511ecf8fe845be4f1c6
lichongrui <858998723@qq.com>
2021-12-28 15:14:19 +0800

    1.0.0

---
+++ docs/.vuepress/config/navbar.js
@@ -0,0 +1,75 @@
+module.exports = [  
+    { text: '首页', link: '/', icon: "fas fa-house-user"},
+    { 
+        text: '地理与气象分析库', 
+        link: '/01.gma/',
+        icon:"fas fa-book-reader",
+        items: [
+            {
+                text: '开始', 
+                items:[
+                    { text: '简介', link:'/01.gma/1.Introduce'},
+                    { text: '安装和引用', link:'/01.gma/2.Setup'}
+                 ]
+             },
+            {
+                text: '函数和功能', 
+                items:[
+                    {text:'库组织方式', link: '/01.gma/3.Structure'},
+                    {text:'函数功能列表', link: '/01.gma/4.Function'},
+                    {text:'默认设置', link: '/01.gma/5.Default'},
+                    {text:'使用函数', link: '/01.gma/6.Use'},
+                ]
+                
+             },
+            {
+                text: '更新',
+                items:[
+                    {text:'更新计划', link: '/01.gma/7.Plan'},
+                    {text:'更新日志', link: '/01.gma/8.Update'},
+                ]
+             },
+        ]
+    },
+    {
+        text: '地信研学',
+        link: '/02.geography/',
+        icon: "fas fa-globe-asia",
+        items: [
+            {
+                text: '坐标系',
+                items: [
+                    { text: '地理坐标系', link: '/02.geography/CoordinateSystem/1.gcs' }
+
+                ]
+
+            }
+        ]
+    },
+    { 
+        text: '气象研学', 
+        link: '/03.meteorology/', 
+        icon: "fas fa-cloud-sun-rain",
+        items:[
+            {
+                text:'插值', 
+                items:[
+                    {text:'ANUSPLIN', link:'/03.meteorology/ANUSPLIN/1.Start'}
+                ]
+            }
+        ]
+        
+    },
+    { text: '遥感研学', link: '/04.RS/', icon:"fas fa-satellite"},
+    { text: '资源教程', link: '/05.resources/', icon:"fab fa-sourcetree"},
+    { text: '地理人文', link: '/06.literature/', icon:"fab fa-artstation"},
+    {
+        text:'探索',
+        icon:"fab fa-wpexplorer",
+        items:[
+            { text: '时间线', link: '/timeline/', icon:"reco-date"},
+            { text: '分类', link: '/categories/地理与气象分析库/', icon:"reco-category"},
+            { text: '标签', link: '/tag/', icon: 'reco-tag' },
+        ]      
+    }
+]
---
+++ docs/.vuepress/config/sidebar.js
@@ -0,0 +1,69 @@
+module.exports = {
+    '/01.gma/': [
+        {
+            title: '开始', 
+            collapsable: false, 
+            children: [
+                '1.Introduce', 
+                '2.Setup'
+            ]
+        },
+        {
+            title: '函数和功能', 
+            collapsable: false, 
+            children: [
+                '3.Structure', 
+                '4.Function', 
+                '5.Default',
+                '6.Use'
+            ]
+        },
+        {
+            title: '更新', 
+            collapsable: false, 
+            children: [
+                '7.Plan', 
+                '8.Update'
+            ]
+        }
+    ],
+    '/02.geography/': [
+        {
+            title: '坐标系', 
+            collapsable: false, 
+            children: [
+                'CoordinateSystem/1.gcs', 
+            ]
+        }
+    ],
+    '/03.meteorology/ANUSPLIN/': [
+        {
+            title:'插值',
+            collapsable: false, 
+            children:[
+                {
+                    title: 'ANUSPLIN',
+                    collapsable: true, 
+                    children: [
+                        '1.Start',
+                        '2.Description',
+                        '3.IPProcess',
+                        '4.DataPP', 
+                        '5.DEMPP', 
+                        '6.Scripting'
+                    ]
+                }
+            ]
+        }
+    ],
+    '/05.resources/': [
+        {
+            title: 'Python',
+            collapsable: false,//true, 
+            children: [
+                'Python/0.GetPath',
+                'Python/1.FillNoData',
+            ]
+        }
+    ]
+}
